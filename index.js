34047d5f0de4039781af676abd95ab68ed9d701c
Sim Greenbaum <gbs4ever2@gmail.com>
2019-05-02 21:17:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (n + parseInt(lis[i].innerHTML)).toString()
+    lis[i].innerHTML = n + parseInt(lis[i].innerHTML)
 
   //  lis[i].innerHTML = (i + n).toString();
   }
